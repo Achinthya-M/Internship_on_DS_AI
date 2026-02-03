7f605a27af295e010880b5fcb4d8eb7f2005f8f6
Achinthya-M <achinthyam702@gmail.com>
2026-02-03 14:35:10 +0530

    Day 2:Added new files

---
+++ src/day2fundamentals.py
@@ -0,0 +1,55 @@
+age = 15
+height = 5.4
+_name = "Alice"
+is_student = True
+print(type(age))
+print(type(height))
+print(type(_name))
+print(type(is_student))
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+if operation == '+':
+    print("Sum:", a + b)
+elif operation == '-':
+    print("Difference:", a - b)
+elif operation == '*':
+    print("Product:", a * b)
+elif operation == '/':
+    if b != 0:
+        print("Division:", a / b)
+    else:
+        print("Error: Division by zero")
+else:
+    print("Invalid operation")
+
+# Greeting program
+name = input("Enter your name: ")
+print("Hello,"+ name + "!")
+
+# #fstring
+age = input("Enter your age: ")
+print(f"My age is, {age}!")
+
+#task 1
+name = input("Enter your name: ")
+age = int(input("Enter your current age: "))
+print(f"Hey {name}, you will be {age+4} years old in 2030!")
+
+#task2
+total_bill = float(input("Enter total bill amount: "))
+people = int(input("Enter number of people: "))
+share_per_person = total_bill / people
+print(f"Total Bill: {total_bill}. Each person pays: {share_per_person}")
+print(type(total_bill))
+print(type(people))
+print(type(share_per_person))
+
+#task 3
+item_name=str(input("enter the item name:"))
+item_price=int(input("enter the item price:"))
+item_qut=int(input("enter the Qty:"))
+price=item_price*item_qut
+in_stock=True
+print(f"Item:{item_name}, Price:{item_price}, Qty:{item_qut},Total Price:{price},Available:{in_stock}")
---
+++ src/day3fundamentals.py
@@ -0,0 +1,20 @@
+# numbers = [10,20,30,40]
+# coordinates = (5,10)
+# print(numbers)
+# print(coordinates)
+
+# a = [100,200,300,400,500,600,700,800,900]
+# print(a[-3:-1]) 
+# print(a[-1:-3]) 
+# print(a[1:4:2])
+# print(a[1:7:2])
+# print(a[-7:-3:2])
+
+marks = [95,75,85,65]
+marks.append(55)
+print(marks)
+marks.pop()
+marks.sort()
+print(marks)
+marks.reverse()
+print(marks)
